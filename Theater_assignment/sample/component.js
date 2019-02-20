648b8d88ba93eccc576199c7303590fa084604ae
Kawisara  Bunyen <zelotype.3@gmail.com>
2019-02-20 20:37:49 +0700

    bug vue component

---
+++ Theater_assignment/sample/component.js
@@ -0,0 +1,25 @@
+Vue.component("navbar", {
+    template:
+        `
+    <header>
+      <nav style="z-index:1000;">
+          <div class="menu-icon">
+              <i class="fa fa-bars fa-2x"></i>
+          </div>
+          <div class="logo">
+              LOGO
+          </div>
+          <div class="menu">
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </div>
+      </nav>
+    </header>
+    `
+});
