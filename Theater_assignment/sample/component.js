7f7daa439d385e484ae931f4f25dec234cf8f77d
Kawisara  Bunyen <zelotype.3@gmail.com>
2019-02-21 23:43:47 +0700

    add data file and navbar

--- Theater_assignment/sample/component.js
+++ Theater_assignment/sample/component.js
@@ -9,30 +9,34 @@ Vue.component('navbar', {
               <li><a href="#" style="color:orange">Blog</a></li>
               <li><a href="#" style="color:orange">Contact</a></li>
               <li><a href="#" style="color:orange">Contact</a></li>
-              <li><button type='button' class="btn btn-success" data-toggle="modal" data-target="#popUpWindow">Open Log In
-              Window</button></li>
+              <li><button type='button' class="btn btn-success" data-toggle="modal" data-target="#popUpWindow">Sign in</button></li>
           </ul>
   </nav>
   <div class="modal fade" id="popUpWindow">
       <div class="modal-dialog">
           <div class="modal-content">
               <!-- header -->
+              <div class="all-sign-in">
               <div class="modal-header">
-              <h3 class="modal-title">Login Form</h3>
+              <h3 class="modal-title">Sign in</h3>
                   <button type="button" class="close" data-dismiss="modal">&times;</button>
               </div>
               <!-- body -->
-              <div class="modal-header">
+              <div class="form-sign-in">
                   <form role="form" style="width:100%;">
-                      <div class="form-group">
+                      <div class="form-group" style="padding-top: 3%;">
+                        <label>Email address</label>
                           <input type="email" class="form-control" placeholder="Email"/>
+                          <label>Password</label>
                           <input type="password" class="form-control" placeholder="Password" />
+                          <button type="button" class="btn btn-link">Create Account</button>
                       </div>
                   </form>
               </div>
+              </div>
               <!-- footer -->
               <div class="modal-footer">
-                  <button class="btn btn-primary btn-block">Log In</button>
+                  <button class="btn btn-primary btn-block btn-lg">Sign In</button>
               </div>
 
           </div>
