5b3037f657fe6cc72ce005b1883ab47bcbb1c9ac
prapawity <protfc16@gmail.com>
2019-02-21 19:54:32 +0700

    push all fix can't click

--- Theater_assignment/sample/component.js
+++ Theater_assignment/sample/component.js
@@ -1,55 +1,46 @@
 Vue.component('navbar', {
-    template: `
-    <div>
-      <nav style="z-index:1000;">
-          <div class="menu-icon">
-              <i class="fa fa-bars fa-2x"></i>
-          </div>
-          <div class="logo">
-          <img style="width: auto;min-width: 5%; max-width: 10%;" src="https://upload.wikimedia.org/wikipedia/th/0/01/Sf-ipad.png">
-          </div>
-          <div class="menu">
-              <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">Contact</a></li>
-                  <li><a href="#">Contact</a></li>
-                  <li><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Sign in
-</button></li>
-              </ul>
-              
-          </div>
-      </nav>
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Sign in</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+  template: `
+  <div>
+  <nav style="z-index:2;text-align: right;">
+      <div class="menu-icon">
+          <i class="fa fa-bars fa-2x"></i>
       </div>
-      <div class="modal-body">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-      </form>
+      <img class="logo" style="width: auto;min-width: 10%; max-width: 15%;" src="poster/moviehub1kpx.png">
+          <ul style="width:100%">
+              <li><a href="#" style="color:orange">Home</a></li>
+              <li><a href="#" style="color:orange">About</a></li>
+              <li><a href="#" style="color:orange">Blog</a></li>
+              <li><a href="#" style="color:orange">Contact</a></li>
+              <li><a href="#" style="color:orange">Contact</a></li>
+              <li><button type='button' class="btn btn-success" data-toggle="modal" data-target="#popUpWindow">Open Log In
+              Window</button></li>
+          </ul>
+  </nav>
+  <div class="modal fade" id="popUpWindow">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <!-- header -->
+              <div class="modal-header">
+              <h3 class="modal-title">Login Form</h3>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+              </div>
+              <!-- body -->
+              <div class="modal-header">
+                  <form role="form" style="width:100%;">
+                      <div class="form-group">
+                          <input type="email" class="form-control" placeholder="Email"/>
+                          <input type="password" class="form-control" placeholder="Password" />
+                      </div>
+                  </form>
+              </div>
+              <!-- footer -->
+              <div class="modal-footer">
+                  <button class="btn btn-primary btn-block">Log In</button>
+              </div>
+
+          </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Sign in</button>
-      </div>
-    </div>
   </div>
-</div>
-    </div>`
+</div>`
 
 });
