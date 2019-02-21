63fb036d9cc0ea86b17c15ed7f91bb492f48e362
prapawity <protfc16@gmail.com>
2019-02-21 12:49:34 +0700

    add incinema

--- Theater_assignment/sample/component.js
+++ Theater_assignment/sample/component.js
@@ -6,7 +6,7 @@ Vue.component('navbar', {
               <i class="fa fa-bars fa-2x"></i>
           </div>
           <div class="logo">
-              LOGO
+          <img style="width: auto;min-width: 5%; max-width: 10%;" src="https://upload.wikimedia.org/wikipedia/th/0/01/Sf-ipad.png">
           </div>
           <div class="menu">
               <ul>
@@ -15,9 +15,32 @@ Vue.component('navbar', {
                   <li><a href="#">Blog</a></li>
                   <li><a href="#">Contact</a></li>
                   <li><a href="#">Contact</a></li>
-                  <li><a href="#">Contact</a></li>
+                  <li><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button></li>
               </ul>
+              
           </div>
       </nav>
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>`
+
 });
